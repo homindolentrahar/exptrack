fb3929e665d1990e47de89105dca00050158f157
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2021-11-26 22:16:45 +0700

    Disabling JIT mode from Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mode: "jit",
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -15,11 +14,6 @@ module.exports = {
     },
   },
   variants: {
-    extend: {
-      ringWidth: ["hover", "active"],
-      ringColor: ["hover", "active"],
-      ringOpacity: ["hover", "active"],
-    },
+    extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
 };
