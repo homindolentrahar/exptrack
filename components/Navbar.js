a4c8deb37968fc0fffdc96702ecc04cdbc9af403
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2021-11-26 13:26:35 +0700

    Fixing warning on Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flex flex-col items-center gap-12 py-4 border-b border-red-50">
-      <Link href="/">
+      <Link href="/" passHref={true}>
         <div className="flex gap-4 items-center cursor-pointer">
           <Image src="/icons/money.png" width={32} height={32} alt="icon" />
           <h1 className="text-gray-900 text-2xl font-semibold">Exptrack</h1>
