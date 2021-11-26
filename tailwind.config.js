f186ba63b0b909eb84eecee8b8f26a03c1c6ad01
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2021-11-26 22:03:04 +0700

    Updating tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
